0c8d09a0aedc51b67eed5a0847509715bf7f4dae
gzdieheart <dieheart@126.com>
2025-04-01 13:22:28 +0800

    update copyright config module

--- server/src/main/java/com/udeve/config/WxMaConfig.java
+++ server/src/main/java/com/udeve/config/WxMaConfig.java
@@ -1,5 +1,15 @@
 package com.udeve.config;
-
+/**
+ * +----------------------------------------------------------------------
+ * | 友得云客
+ * +----------------------------------------------------------------------
+ * | Copyright (c) 2024~2027
+ * +----------------------------------------------------------------------
+ * | Licensed 友得云客不是自有软件 未经允许不可移除相关版权
+ * +----------------------------------------------------------------------
+ * | Author: www.youdeyunke.com
+ * +----------------------------------------------------------------------
+ */
 import cn.binarywang.wx.miniapp.api.WxMaService;
 import cn.binarywang.wx.miniapp.api.impl.WxMaServiceImpl;
 import cn.binarywang.wx.miniapp.config.impl.WxMaDefaultConfigImpl;
